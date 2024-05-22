4636d2d119fc6c8b330dc2ff77d3473bfe1e8607
= <=>
2024-05-22 18:54:37 +0530

    new

---
+++ A-FORM/assets/js/main.js
@@ -0,0 +1,140 @@
+/* -----------------------------------------------------------------------------
+
+
+
+File:           JS Core
+Version:        1.0
+Last change:    00/00/00 
+-------------------------------------------------------------------------------- */
+(function() {
+
+	"use strict";
+
+	var Wizard = {
+		init: function() {
+			this.Basic.init();  
+		},
+
+		Basic: {
+			init: function() {
+
+				this.preloader();
+				this.NeedJobSlide();
+				this.FileUpload();
+				
+			},
+			preloader: function (){
+				jQuery(window).on('load', function(){
+					jQuery('#preloader').fadeOut('slow',function(){jQuery(this).remove();});
+				});
+			},
+			NeedJobSlide: function (){
+				$('#need-job-slide-id').owlCarousel({
+					margin:30,
+					responsiveClass:true,
+					nav: true,
+					dots: false,
+					loop:true,
+					center: true,
+					autoHeight: true,
+					autoplay: false,
+					navText:["<i class='fas fa-arrow-left'></i>","<i class='fas fa-arrow-right'></i>"],
+					smartSpeed: 1000,
+					responsive:{
+						0:{
+							items:1,
+						},
+						400:{
+							items:1,
+						},
+						600:{
+							items:1,
+						},
+						700:{
+							items:2,
+						},
+						1000:{
+							items:3,
+
+						},
+					},
+				})
+			},
+			FileUpload: function (){
+				$("#customFile").change(function() {
+					filename = this.files[0].name
+				});
+				$(".custom-file-input").on("change", function() {
+					var fileName = $(this).val().split("\\").pop();
+					$(this).siblings(".custom-file-label").addClass("selected").html(fileName);
+				});
+			},
+		}
+	}
+	jQuery(document).ready(function (){
+		Wizard.init();
+	});
+
+
+	// arya===============
+	// -------otp_fild js----------
+	
+	const inputs = document.querySelectorAll(".otp-field input");
+
+inputs.forEach((input, index) => {
+    input.dataset.index = index;
+    input.addEventListener("keyup", handleOtp);
+    input.addEventListener("paste", handleOnPasteOtp);
+});
+
+function handleOtp(e) {
+    /**
+     * <input type="text" 👉 maxlength="1" />
+     * 👉 NOTE: On mobile devices `maxlength` property isn't supported,
+     * So we to write our own logic to make it work. 🙂
+     */
+    const input = e.target;
+    let value = input.value;
+    let isValidInput = value.match(/[0-9a-z]/gi);
+    input.value = "";
+    input.value = isValidInput ? value[0] : "";
+
+    let fieldIndex = input.dataset.index;
+    if (fieldIndex < inputs.length - 1 && isValidInput) {
+        input.nextElementSibling.focus();
+    }
+
+    if (e.key === "Backspace" && fieldIndex > 0) {
+        input.previousElementSibling.focus();
+    }
+
+    if (fieldIndex == inputs.length - 1 && isValidInput) {
+        submit();
+    }
+}
+
+function handleOnPasteOtp(e) {
+    const data = e.clipboardData.getData("text");
+    const value = data.split("");
+    if (value.length === inputs.length) {
+        inputs.forEach((input, index) => (input.value = value[index]));
+        submit();
+    }
+}
+
+function submit() {
+    console.log("Submitting...");
+    // 👇 Entered OTP
+    let otp = "";
+    inputs.forEach((input) => {
+        otp += input.value;
+        input.disabled = true;
+        input.classList.add("disabled");
+    });
+    console.log(otp);
+    // 👉 Call API below
+}
+
+
+
+})();
